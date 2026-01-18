5759605f85b99ce058b0d5da8aa28049acef9735
Sagnik Jana <sagnikjana2001@gmail.com>
2026-01-18 19:56:52 +0530

    add response schema

--- server/app/agents/elk_input_graph_generator_agent/agent.py
+++ server/app/agents/elk_input_graph_generator_agent/agent.py
@@ -1,4 +1,11 @@
 from langchain.agents import create_agent
 from app.config.settings import settings
+from app.agents.elk_input_graph_generator_agent.tools import search_aws_icons
+from app.agents.elk_input_graph_generator_agent.schemas import Graph
 
-agent = create_agent(model=settings.DEFAULT_CHAT_MODEL_NAME, tools=[], verbose=True)
+agent = create_agent(
+    model=settings.DEFAULT_CHAT_MODEL_NAME,
+    tools=[search_aws_icons],
+    verbose=True,
+    response_format=Graph,
+)
--- server/app/agents/elk_input_graph_generator_agent/schemas.py
+++ server/app/agents/elk_input_graph_generator_agent/schemas.py
@@ -2,17 +2,30 @@ from __future__ import annotations
 from typing import List, Optional
 from pydantic import BaseModel, Field
 
+
 class Edge(BaseModel):
-    id: str = Field(description="Unique identifier for the edge")
-    sources: List[str] = Field(description="List of source node identifiers")
-    targets: List[str] = Field(description="List of target node identifiers")
+    id: str = Field(description="Unique id for the edge")
+    sources: List[str] = Field(description="List of source node ids")
+    targets: List[str] = Field(description="List of target node ids")
+
 
 class Node(BaseModel):
-    id: str = Field(description="Unique identifier for the node")
-    text: Optional[str] = Field(default=None, description="Optional text content to be displayed on the node")
-    icon_id: Optional[str] = Field(default=None, description="Optional identifier for an icon to be displayed")
-    children: Optional[List[Node]] = Field(default=None, description="List of direct children nodes found inside this node. This structure is recursive.")
-    edges: Optional[List[Edge]] = Field(default=None, description="List of edges contained within this node scope.")
+    id: str = Field(description="Unique id for the node")
+    text: Optional[str] = Field(
+        default=None, description="Optional text content to be displayed on the node"
+    )
+    icon_id: Optional[str] = Field(
+        default=None, description="Optional identifier for an icon to be displayed"
+    )
+    children: Optional[List[Node]] = Field(
+        default=None,
+        description="List of direct children nodes found inside this node. This structure is recursive.",
+    )  # rebuild the model to update forward references for the recursive 'children' field
 
-# rebuild the model to update forward references for the recursive 'children' field
+
 Node.model_rebuild()
+
+
+class Graph(BaseModel):
+    nodes: List[Node] = Field(description="List of nodes in the graph")
+    edges: List[Edge] = Field(description="List of All the edges in the graph")
