3d2132a49b16eee942defb337851e67821f59cb5
Sagnik Jana <sagnikjana2001@gmail.com>
2026-02-28 10:12:43 +0530

    removed unused code

--- server/app/agents/elk_input_graph_generator_agent/agent.py
+++ server/app/agents/elk_input_graph_generator_agent/agent.py
@@ -1,9 +1,7 @@
 from langchain.agents import create_agent
-from app.config.settings import settings
 from app.agents.elk_input_graph_generator_agent.tools import search_aws_icons
 from app.agents.elk_input_graph_generator_agent.schemas import Graph
 from app.agents.elk_input_graph_generator_agent.prompts import SYSTEM_PROMPT
-from typing import Any
 from app.agents.elk_input_graph_generator_agent.chat_models import default_chat_model
 from langchain.agents.structured_output import ToolStrategy
 
